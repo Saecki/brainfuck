72e2204642f4d7d57c2b4afbf834c679ae2b137b
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2024-07-07 10:35:10 +0200

    perf: use optimized instruction enum
    - combine shift and increment/decrement instructions
    - store jump indices to matching parentheses

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use std::cmp::PartialOrd;
 use std::env::args;
 use std::io::{Read, Write};
-use std::cmp::PartialOrd;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum Token {
@@ -14,102 +14,116 @@ enum Token {
     RSquare,
 }
 
+impl Token {
+    fn is_combinable(self) -> bool {
+        match self {
+            Token::Shl | Token::Shr | Token::Inc | Token::Dec => true,
+            Token::Output | Token::Input | Token::LSquare | Token::RSquare => false,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum Instruction {
+    Shl(u16),
+    Shr(u16),
+    Inc(u8),
+    Dec(u8),
+    Output,
+    Input,
+    LSquare(u32),
+    RSquare(u32),
+}
+
 fn main() {
     let path = args().skip(1).next().unwrap();
     let input = std::fs::read_to_string(&path).unwrap();
     let bytes = input.as_bytes();
 
-    let mut token_cursor = 0;
+    let mut ip = 0;
 
     const LEN: usize = 30000;
     let mut pointer: usize = 0;
     let mut registers: [u8; LEN] = [0; LEN];
 
-    let tokens = bytes.iter().filter_map(|b| {
-        let t = match *b {
-            b'<' => Token::Shl,
-            b'>' => Token::Shr,
-            b'+' => Token::Inc,
-            b'-' => Token::Dec,
-            b'.' => Token::Output,
-            b',' => Token::Input,
-            b'[' => Token::LSquare,
-            b']' => Token::RSquare,
-            _ => return None,
-        };
-        Some(t)
-    }).collect::<Vec<_>>();
+    let tokens = bytes
+        .iter()
+        .filter_map(|b| {
+            let t = match *b {
+                b'<' => Token::Shl,
+                b'>' => Token::Shr,
+                b'+' => Token::Inc,
+                b'-' => Token::Dec,
+                b'.' => Token::Output,
+                b',' => Token::Input,
+                b'[' => Token::LSquare,
+                b']' => Token::RSquare,
+                _ => return None,
+            };
+            Some(t)
+        })
+        .collect::<Vec<_>>();
+
+    let mut instructions = tokens
+        .chunk_by(|a, b| a.is_combinable() && a == b)
+        .map(|chunk| match chunk[0] {
+            Token::Shl => Instruction::Shl(chunk.len() as u16),
+            Token::Shr => Instruction::Shr(chunk.len() as u16),
+            Token::Inc => Instruction::Inc(chunk.len() as u8),
+            Token::Dec => Instruction::Dec(chunk.len() as u8),
+            Token::Output => Instruction::Output,
+            Token::Input => Instruction::Input,
+            Token::LSquare => Instruction::LSquare(0),
+            Token::RSquare => Instruction::RSquare(0),
+        })
+        .collect::<Vec<_>>();
+
+    let mut par_stack = Vec::new();
+    for (i, instruction) in instructions.iter_mut().enumerate() {
+        match instruction {
+            Instruction::LSquare(closing_idx_ref) => par_stack.push((i, closing_idx_ref)),
+            Instruction::RSquare(opening_idx_ref) => {
+                let Some((opening_idx, closing_idx_ref)) = par_stack.pop() else {
+                    unreachable!("mismatched parentheses")
+                };
+
+                *opening_idx_ref = opening_idx as u32 + 1;
+                *closing_idx_ref = i as u32 + 1;
+            }
+            _ => (),
+        }
+    }
 
     loop {
-        let Some(b) = tokens.get(token_cursor) else {
+        let Some(b) = instructions.get(ip) else {
             break;
         };
 
         match *b {
-            Token::Shl => {
-                pointer -= 1;
-            }
-            Token::Shr => {
-                pointer += 1;
-            }
-            Token::Inc => {
-                registers[pointer] += 1;
-            }
-            Token::Dec => {
-                registers[pointer] -= 1;
-            }
-            Token::Output => {
+            Instruction::Shl(n) => pointer -= n as usize,
+            Instruction::Shr(n) => pointer += n as usize,
+            Instruction::Inc(n) => registers[pointer] += n,
+            Instruction::Dec(n) => registers[pointer] -= n,
+            Instruction::Output => {
                 _ = std::io::stdout().write(&registers[pointer..pointer + 1]);
             }
-            Token::Input => {
+            Instruction::Input => {
                 _ = std::io::stdin().read(&mut registers[pointer..pointer + 1]);
             }
-            Token::LSquare => {
+            Instruction::LSquare(idx) => {
                 if registers[pointer] == 0 {
-                    let mut par_stack = 1;
-                    token_cursor += 1;
-                    for (i, b) in tokens[token_cursor..].iter().enumerate() {
-                        match b {
-                            Token::LSquare => {
-                                par_stack += 1;
-                            }
-                            Token::RSquare => {
-                                par_stack -= 1;
-                                if par_stack == 0 {
-                                    token_cursor += i;
-                                    break;
-                                }
-                            }
-                            _ => (),
-                        }
-                    }
+                    ip = idx as usize;
                     continue;
                 }
             }
-            Token::RSquare => {
+            Instruction::RSquare(idx) => {
                 if registers[pointer] > 0 {
-                    let mut par_stack = 1;
-                    token_cursor -= 1;
-                    for (i, b) in tokens[..=token_cursor].iter().rev().enumerate() {
-                        match b {
-                            Token::RSquare => {
-                                par_stack += 1;
-                            }
-                            Token::LSquare => {
-                                par_stack -= 1;
-                                if par_stack == 0 {
-                                    token_cursor -= i;
-                                    break;
-                                }
-                            }
-                            _ => (),
-                        }
-                    }
+                    ip = idx as usize;
                     continue;
                 }
             }
         }
 
-        token_cursor += 1;
+        ip += 1;
     }
 }
