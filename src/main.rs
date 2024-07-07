c2cc4f3a8cbe5abdbefbaacb4ba9c94f4f34d5bb
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2024-07-07 11:18:23 +0200

    feat: print a bunch of stats and representations

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,21 @@ enum Token {
     RSquare,
 }
 
+impl std::fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Token::Shl => write!(f, "<"),
+            Token::Shr => write!(f, ">"),
+            Token::Inc => write!(f, "+"),
+            Token::Dec => write!(f, "-"),
+            Token::Output => write!(f, "."),
+            Token::Input => write!(f, ","),
+            Token::LSquare => write!(f, "["),
+            Token::RSquare => write!(f, "]"),
+        }
+    }
+}
+
 impl Token {
     fn is_combinable(self) -> bool {
         match self {
@@ -35,6 +50,23 @@ enum Instruction {
     Input,
     LSquare(u32),
     RSquare(u32),
+}
+
+impl std::fmt::Display for Instruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Instruction::Shl(n) => write!(f, "< ({n})"),
+            Instruction::Shr(n) => write!(f, "> ({n})"),
+            Instruction::Inc(n) => write!(f, "+ ({n})"),
+            Instruction::Dec(n) => write!(f, "- ({n})"),
+            Instruction::OffsetInc(i, n) => write!(f, "<{i}> + ({n})"),
+            Instruction::OffsetDec(i, n) => write!(f, "<{i}> - ({n})"),
+            Instruction::Output => write!(f, "out"),
+            Instruction::Input => write!(f, "in"),
+            Instruction::LSquare(_) => write!(f, "["),
+            Instruction::RSquare(_) => write!(f, "]"),
+        }
+    }
 }
 
 fn main() {
@@ -69,6 +101,11 @@ fn main() {
     // combine instructions
     let mut instructions = tokens
         .chunk_by(|a, b| a.is_combinable() && a == b)
+        .inspect(|c| {
+            if c.len() > 1 {
+                println!("combine {}", c.len());
+            }
+        })
         .map(|chunk| match chunk[0] {
             Token::Shl => Instruction::Shl(chunk.len() as u16),
             Token::Shr => Instruction::Shr(chunk.len() as u16),
@@ -80,8 +117,42 @@ fn main() {
             Token::RSquare => Instruction::RSquare(0),
         })
         .collect::<Vec<_>>();
+    println!("========================================");
+    println!(
+        "tokens before {} after: {} ({:.3}%)",
+        tokens.len(),
+        instructions.len(),
+        100.0 * instructions.len() as f32 / tokens.len() as f32,
+    );
+
+    let mut indent = 0;
+    for i in instructions.iter() {
+        if let Instruction::RSquare(_) = i {
+            indent -= 1
+        }
+        for _ in 0..indent {
+            print!("    ");
+        }
+        match i {
+            Instruction::Shl(n) => println!("{:<<width$}", "", width=*n as usize),
+            Instruction::Shr(n) => println!("{:><width$}", "", width=*n as usize),
+            Instruction::Inc(n) => println!("{:+<width$}", "", width=*n as usize),
+            Instruction::Dec(n) => println!("{:-<width$}", "", width=*n as usize),
+            Instruction::OffsetInc(_, _) => unreachable!(),
+            Instruction::OffsetDec(_, _) => unreachable!(),
+            Instruction::Output => println!("."),
+            Instruction::Input => println!(","),
+            Instruction::LSquare(_) => println!("["),
+            Instruction::RSquare(_) => println!("]"),
+        }
+        if let Instruction::LSquare(_) = i {
+            indent += 1
+        }
+    }
+
 
     // offset instructions
+    let prev_len = instructions.len();
     {
         let mut i = 0;
         while i + 3 < instructions.len() - 3 {
@@ -121,6 +192,13 @@ fn main() {
             i += 1;
         }
     }
+    println!("========================================");
+    println!(
+        "instructions before {} after: {} ({:.3}%)",
+        prev_len,
+        instructions.len(),
+        100.0 * instructions.len() as f32 / tokens.len() as f32,
+    );
 
     // update jump indices
     let mut par_stack = Vec::new();
@@ -129,13 +207,27 @@ fn main() {
             Instruction::LSquare(closing_idx_ref) => par_stack.push((i, closing_idx_ref)),
             Instruction::RSquare(opening_idx_ref) => {
                 let Some((opening_idx, closing_idx_ref)) = par_stack.pop() else {
-                    unreachable!("mismatched parentheses")
+                    unreachable!("mismatched brackets")
                 };
 
                 *opening_idx_ref = opening_idx as u32 + 1;
                 *closing_idx_ref = i as u32 + 1;
             }
             _ => (),
+        }
+    }
+
+    let mut indent = 0;
+    for i in instructions.iter() {
+        if let Instruction::RSquare(_) = i {
+            indent -= 1
+        }
+        for _ in 0..indent {
+            print!("    ");
+        }
+        println!("{i}");
+        if let Instruction::LSquare(_) = i {
+            indent += 1
         }
     }
 
