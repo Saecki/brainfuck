bd46231d2d734f94fc6f5c9afa669d290becef77
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2024-07-07 10:00:43 +0200

    refactor: parse into tokens

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,82 @@
 use std::env::args;
 use std::io::{Read, Write};
+use std::cmp::PartialOrd;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum Token {
+    Shl,
+    Shr,
+    Inc,
+    Dec,
+    Output,
+    Input,
+    LSquare,
+    RSquare,
+}
 
 fn main() {
     let path = args().skip(1).next().unwrap();
     let input = std::fs::read_to_string(&path).unwrap();
     let bytes = input.as_bytes();
 
-    let mut byte_cursor = 0;
+    let mut token_cursor = 0;
 
     const LEN: usize = 30000;
     let mut pointer: usize = 0;
     let mut registers: [u8; LEN] = [0; LEN];
 
+    let tokens = bytes.iter().filter_map(|b| {
+        let t = match *b {
+            b'<' => Token::Shl,
+            b'>' => Token::Shr,
+            b'+' => Token::Inc,
+            b'-' => Token::Dec,
+            b'.' => Token::Output,
+            b',' => Token::Input,
+            b'[' => Token::LSquare,
+            b']' => Token::RSquare,
+            _ => return None,
+        };
+        Some(t)
+    }).collect::<Vec<_>>();
+
     loop {
-        let Some(b) = bytes.get(byte_cursor) else {
+        let Some(b) = tokens.get(token_cursor) else {
             break;
         };
 
         match *b {
-            b'<' => {
+            Token::Shl => {
                 pointer -= 1;
             }
-            b'>' => {
+            Token::Shr => {
                 pointer += 1;
             }
-            b'+' => {
+            Token::Inc => {
                 registers[pointer] += 1;
             }
-            b'-' => {
+            Token::Dec => {
                 registers[pointer] -= 1;
             }
-            b'.' => {
+            Token::Output => {
                 _ = std::io::stdout().write(&registers[pointer..pointer + 1]);
             }
-            b',' => {
+            Token::Input => {
                 _ = std::io::stdin().read(&mut registers[pointer..pointer + 1]);
             }
-            b'[' => {
+            Token::LSquare => {
                 if registers[pointer] == 0 {
                     let mut par_stack = 1;
-                    byte_cursor += 1;
-                    for (i, b) in bytes[byte_cursor..].iter().enumerate() {
+                    token_cursor += 1;
+                    for (i, b) in tokens[token_cursor..].iter().enumerate() {
                         match b {
-                            b'[' => {
+                            Token::LSquare => {
                                 par_stack += 1;
                             }
-                            b']' => {
+                            Token::RSquare => {
                                 par_stack -= 1;
                                 if par_stack == 0 {
-                                    byte_cursor += i;
+                                    token_cursor += i;
                                     break;
                                 }
                             }
@@ -58,19 +86,19 @@ fn main() {
                     continue;
                 }
             }
-            b']' => {
+            Token::RSquare => {
                 if registers[pointer] > 0 {
                     let mut par_stack = 1;
-                    byte_cursor -= 1;
-                    for (i, b) in bytes[..=byte_cursor].iter().rev().enumerate() {
+                    token_cursor -= 1;
+                    for (i, b) in tokens[..=token_cursor].iter().rev().enumerate() {
                         match b {
-                            b']' => {
+                            Token::RSquare => {
                                 par_stack += 1;
                             }
-                            b'[' => {
+                            Token::LSquare => {
                                 par_stack -= 1;
                                 if par_stack == 0 {
-                                    byte_cursor -= i;
+                                    token_cursor -= i;
                                     break;
                                 }
                             }
@@ -80,9 +108,8 @@ fn main() {
                     continue;
                 }
             }
-            _ => (),
         }
 
-        byte_cursor += 1;
+        token_cursor += 1;
     }
 }
