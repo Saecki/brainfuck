83df4440f9bae6769ac9f2112ae25cc59d292f9e
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2024-07-13 12:26:38 +0200

    docs(x86): update and add some missing documentation

--- src/x86.rs
+++ src/x86.rs
@@ -1,10 +1,60 @@
 use crate::{Instruction, NUM_REGISTERS};
 
+#[repr(C)]
+pub struct ElfFileHeader {
+    pub ei_magic: [u8; 4],
+    pub ei_class: u8,
+    pub ei_data: u8,
+    pub ei_version: u8,
+    pub ei_osabi: u8,
+    pub ei_abiversion: u8,
+    pub ei_pad: [u8; 7],
+
+    pub e_type: u16,
+    pub e_machine: u16,
+    pub e_version: u32,
+    pub e_entry: u64,
+    /// program header offset
+    pub e_phoff: u64,
+    /// section header offset
+    pub e_shoff: u64,
+    pub e_flags: u32,
+    /// size of this header
+    pub e_ehsize: u16,
+    /// program header entry size
+    pub e_phentsize: u16,
+    /// number of program header table entries
+    pub e_phnum: u16,
+    /// section header entry size
+    pub e_shentsize: u16,
+    /// number of section header table entries
+    pub e_shnum: u16,
+    /// index of section header table entry that contains section names
+    pub e_shstrndx: u16,
+}
+
+#[repr(C)]
+pub struct ElfProgramHeader {
+    pub p_type: u32,
+    pub p_flags: u32,
+    pub p_offset: u64,
+    pub p_vaddr: u64,
+    pub p_paddr: u64,
+    pub p_filesz: u64,
+    pub p_memsz: u64,
+    pub p_align: u64,
+}
+
+/// Address mode
 #[derive(Clone, Copy)]
 pub enum ModRm {
+    /// Use address stored in the register or the [`Sib`] to read value from memory
     Indirect(RmI),
+    /// Use address stored in the register or the [`Sib`] + a i8 displacement to read value from memory
     IndirectDisp8(RmID),
+    /// Use address stored in the register or the [`Sib`] + a i32 displacement to read value from memory
     IndirectDisp32(RmID),
+    /// Use value stored in the register
     Register(Reg),
 }
 
@@ -35,7 +85,9 @@ pub enum RmI {
     RegRcx = 0b001,
     RegRdx = 0b010,
     RegRbx = 0b011,
+    /// Use a scaled index byte [`Sib`] following the opcode
     Sib = 0b100,
+    /// Use *only* a constant 32-bit displacement
     Disp32 = 0b101,
     RegRsi = 0b110,
     RegRdi = 0b111,
@@ -76,11 +128,19 @@ pub const fn modrm_ext(modrm: ModRm, ext: u8) -> u8 {
     (modrm.mode() << 6) | (ext << 3) | modrm.rm()
 }
 
+/// Calculate a memory address using the values stored inside the [`Sib::base`] register plus the
+/// [`Sib::idx`] register multiplied by a [`Scale`] factor:
+///
+/// `base + scale * idx`
+///
+/// or
+///
+/// `base[scale * idx]`
 #[derive(Clone, Copy)]
 pub struct Sib {
-    scale: Scale,
-    idx: Reg,
-    base: Reg,
+    pub scale: Scale,
+    pub idx: Reg,
+    pub base: Reg,
 }
 
 impl Sib {
@@ -93,6 +153,7 @@ impl Sib {
     }
 }
 
+/// [`Sib::scale`]
 #[derive(Clone, Copy)]
 pub enum Scale {
     /// 1 byte scale
@@ -103,51 +164,6 @@ pub enum Scale {
     B4 = 0b10,
     /// 8 byte scale
     B8 = 0b11,
-}
-
-#[repr(C)]
-struct ElfFileHeader {
-    ei_magic: [u8; 4],
-    ei_class: u8,
-    ei_data: u8,
-    ei_version: u8,
-    ei_osabi: u8,
-    ei_abiversion: u8,
-    ei_pad: [u8; 7],
-
-    e_type: u16,
-    e_machine: u16,
-    e_version: u32,
-    e_entry: u64,
-    /// program header offset
-    e_phoff: u64,
-    /// section header offset
-    e_shoff: u64,
-    e_flags: u32,
-    /// size of this header
-    e_ehsize: u16,
-    /// program header entry size
-    e_phentsize: u16,
-    /// number of program header table entries
-    e_phnum: u16,
-    /// section header entry size
-    e_shentsize: u16,
-    /// number of section header table entries
-    e_shnum: u16,
-    /// index of section header table entry that contains section names
-    e_shstrndx: u16,
-}
-
-#[repr(C)]
-struct ElfProgramHeader {
-    p_type: u32,
-    p_flags: u32,
-    p_offset: u64,
-    p_vaddr: u64,
-    p_paddr: u64,
-    p_filesz: u64,
-    p_memsz: u64,
-    p_align: u64,
 }
 
 #[macro_export]
@@ -204,7 +220,7 @@ pub fn compile(instructions: &[Instruction]) -> Vec<u8> {
         let header = ElfProgramHeader {
             p_type: 0x0000_0001,                      // loadable segment
             p_flags: 0x0000_0007,                     // read write execute
-            p_offset: PROGRAM_OFFSET as u64,          // (includes the whole file)
+            p_offset: PROGRAM_OFFSET as u64,          // loadable segment starts after this header
             p_vaddr: (VADDR + PROGRAM_OFFSET) as u64, // virtual address to place the segment at
             p_paddr: 0,                               // physical address is not used
             p_filesz: 0,     // size of the whole file (write after code generation)
@@ -259,13 +275,11 @@ fn write_instructions(code: &mut Vec<u8>, instructions: &[Instruction]) {
         write(code, jnz_rel8(rel_jump))
     }
 
-    // generate code
-
     // scaled index byte used to index into the brainfuck register array
     const SIB: Sib = Sib::new(Scale::B1, Reg::Rcx, Reg::Rsp);
 
-    // stores if the jump is redundant, and the location before opening jump (`[`), the jump offset
-    // is stored inside the [6..10] bytes after that
+    // stores if the jump is redundant, and the location after the opening jump (`[`), the jump
+    // offset is stored inside the 4 bytes before that
     let mut jump_stack = Vec::new();
     for inst in instructions.iter() {
         match *inst {
@@ -435,67 +449,79 @@ const REXW: u8 = 0x48;
 //                   ADD
 // ========================================
 
-// `00 /r` : `ADD r/m8 r8` : add r8 to r/m8
+// 8-bit
+
+/// `00 /r` : `ADD r/m8 r8` : add r8 to r/m8
 pub const fn add_r8_to_r8(src: Reg, dest: Reg) -> [u8; 2] {
     let modrm = modrm_reg(ModRm::Register(dest), src);
     [0x00, modrm]
 }
+/// `00 /r` : `ADD r/m8 r8` : add r8 to r/m8
 pub const fn add_r8_to_sib8(src: Reg, dest: Sib) -> [u8; 3] {
     let modrm = modrm_reg(ModRm::Indirect(RmI::Sib), src);
     [0x00, modrm, dest.sib()]
 }
+/// `00 /r` : `ADD r/m8 r8` : add r8 to r/m8
 pub const fn add_r8_to_sib8_disp8(src: Reg, dest: Sib, disp: i8) -> [u8; 4] {
     let modrm = modrm_reg(ModRm::IndirectDisp8(RmID::Sib), src);
     let [disp] = i8::to_le_bytes(disp);
     [0x00, modrm, dest.sib(), disp]
 }
+/// `00 /r` : `ADD r/m8 r8` : add r8 to r/m8
 pub const fn add_r8_to_sib8_disp32(src: Reg, dest: Sib, disp: i32) -> [u8; 7] {
     let modrm = modrm_reg(ModRm::IndirectDisp32(RmID::Sib), src);
     let [b0, b1, b2, b3] = i32::to_le_bytes(disp);
     [0x00, modrm, dest.sib(), b0, b1, b2, b3]
 }
 
-// `80 /0 ib`: `ADD r/m8 imm8` : add imm8 to r/m8
+/// `80 /0 ib`: `ADD r/m8 imm8` : add imm8 to r/m8
 pub const fn add_imm8_to_r8(dest: Reg, ib: u8) -> [u8; 3] {
     let modrm = modrm_ext(ModRm::Register(dest), 0);
     [0x80, modrm, ib]
 }
+/// `80 /0 ib`: `ADD r/m8 imm8` : add imm8 to r/m8
 pub const fn add_imm8_to_sib8(dest: Sib, ib: u8) -> [u8; 4] {
     const MODRM: u8 = modrm_ext(ModRm::Indirect(RmI::Sib), 0);
     [0x80, MODRM, dest.sib(), ib]
 }
+/// `80 /0 ib`: `ADD r/m8 imm8` : add imm8 to r/m8
 pub const fn add_imm8_to_sib8_disp8(dest: Sib, disp: i8, ib: u8) -> [u8; 5] {
     const MODRM: u8 = modrm_ext(ModRm::IndirectDisp8(RmID::Sib), 0);
     let [disp] = i8::to_le_bytes(disp);
     [0x80, MODRM, dest.sib(), disp, ib]
 }
+/// `80 /0 ib`: `ADD r/m8 imm8` : add imm8 to r/m8
 pub const fn add_imm8_to_sib8_disp32(dest: Sib, disp: i32, ib: u8) -> [u8; 8] {
     const MODRM: u8 = modrm_ext(ModRm::IndirectDisp32(RmID::Sib), 0);
     let [b0, b1, b2, b3] = i32::to_le_bytes(disp);
     [0x80, MODRM, dest.sib(), b0, b1, b2, b3, ib]
 }
 
-// `83 /0 ib` : `ADD r/m32 imm8` : add imm8 sign extended to 32-bits to r/m32
+// 32-bit
+
+/// `83 /0 ib` : `ADD r/m32 imm8` : add imm8 sign extended to 32-bits to r/m32
 pub const fn add_imm8_to_r32(dest: Reg, ib: i8) -> [u8; 3] {
     let modrm = modrm_ext(ModRm::Register(dest), 0);
     let [ib] = i8::to_le_bytes(ib);
     [0x83, modrm, ib]
 }
 
-// `81 /0 id` : `ADD r/m32 imm32` : add imm32 to r/m32
+/// `81 /0 id` : `ADD r/m32 imm32` : add imm32 to r/m32
 pub const fn add_imm32_to_r32(dest: Reg, id: i32) -> [u8; 6] {
     let modrm = modrm_ext(ModRm::Register(dest), 0);
     let [b0, b1, b2, b3] = i32::to_le_bytes(id);
     [0x81, modrm, b0, b1, b2, b3]
 }
 
-// `REX.W 01 /r` : `ADD r/m64 r64` : add r64 to r/m64
+// 64-bit
+
+/// `REX.W 01 /r` : `ADD r/m64 r64` : add r64 to r/m64
 pub const fn add_r64_to_r64(src: Reg, dest: Reg) -> [u8; 3] {
     let modrm = modrm_reg(ModRm::Register(dest), src);
     [REXW, 0x01, modrm]
 }
 
-// `REX.W 81 /0 id` : `ADD r/m64 imm32` : add imm32 sign extended to 64-bits to r/m64
+/// `REX.W 81 /0 id` : `ADD r/m64 imm32` : add imm32 sign extended to 64-bits to r/m64
 pub const fn add_imm32_to_r64(dest: Reg, id: i32) -> [u8; 7] {
     let modrm = modrm_ext(ModRm::Register(dest), 0);
     let [b0, b1, b2, b3] = i32::to_le_bytes(id);
@@ -506,61 +532,73 @@ pub const fn add_imm32_to_r64(dest: Reg, id: i32) -> [u8; 7] {
 //                   SUB
 // ========================================
 
-// `28 /r` : `SUB r/m8 r8` : subtract r8 from r/m8
+// 8-bit
+
+/// `28 /r` : `SUB r/m8 r8` : subtract r8 from r/m8
 pub const fn sub_r8_from_r8(src: Reg, sib: Sib) -> [u8; 3] {
     let modrm = modrm_reg(ModRm::Indirect(RmI::Sib), src);
     [0x28, modrm, sib.sib()]
 }
+/// `28 /r` : `SUB r/m8 r8` : subtract r8 from r/m8
 pub const fn sub_r8_from_sib8(src: Reg, dest: Sib) -> [u8; 3] {
     let modrm = modrm_reg(ModRm::Indirect(RmI::Sib), src);
     [0x28, modrm, dest.sib()]
 }
+/// `28 /r` : `SUB r/m8 r8` : subtract r8 from r/m8
 pub const fn sub_r8_from_sib8_disp8(src: Reg, dest: Sib, disp: i8) -> [u8; 4] {
     let modrm = modrm_reg(ModRm::IndirectDisp8(RmID::Sib), src);
     let [disp] = i8::to_le_bytes(disp);
     [0x28, modrm, dest.sib(), disp]
 }
+/// `28 /r` : `SUB r/m8 r8` : subtract r8 from r/m8
 pub const fn sub_r8_from_sib8_disp32(src: Reg, dest: Sib, disp: i32) -> [u8; 7] {
     let modrm = modrm_reg(ModRm::IndirectDisp32(RmID::Sib), src);
     let [b0, b1, b2, b3] = i32::to_le_bytes(disp);
     [0x28, modrm, dest.sib(), b0, b1, b2, b3]
 }
 
-// `80 /5 ib`: `SUB r/m8 imm8` : subtract imm8 from r/m8
+/// `80 /5 ib`: `SUB r/m8 imm8` : subtract imm8 from r/m8
 pub const fn sub_imm8_from_r8(dest: Reg, ib: u8) -> [u8; 3] {
     let modrm = modrm_ext(ModRm::Register(dest), 5);
     [0x80, modrm, ib]
 }
+/// `80 /5 ib`: `SUB r/m8 imm8` : subtract imm8 from r/m8
 pub const fn sub_imm8_from_sib8(dest: Sib, ib: u8) -> [u8; 4] {
     const MODRM: u8 = modrm_ext(ModRm::Indirect(RmI::Sib), 5);
     [0x80, MODRM, dest.sib(), ib]
 }
+/// `80 /5 ib`: `SUB r/m8 imm8` : subtract imm8 from r/m8
 pub const fn sub_imm8_from_sib8_disp8(dest: Sib, disp: i8, ib: u8) -> [u8; 5] {
     const MODRM: u8 = modrm_ext(ModRm::IndirectDisp8(RmID::Sib), 5);
     let [disp] = i8::to_le_bytes(disp);
     [0x80, MODRM, dest.sib(), disp, ib]
 }
+/// `80 /5 ib`: `SUB r/m8 imm8` : subtract imm8 from r/m8
 pub const fn sub_imm8_from_sib8_disp32(dest: Sib, disp: i32, ib: u8) -> [u8; 8] {
     const MODRM: u8 = modrm_ext(ModRm::IndirectDisp32(RmID::Sib), 5);
     let [b0, b1, b2, b3] = i32::to_le_bytes(disp);
     [0x80, MODRM, dest.sib(), b0, b1, b2, b3, ib]
 }
 
-// `83 /5 ib` : `SUB r/m32 imm8` : subtract imm8 sign extended to 32-bit from r/m32
+// 32-bit
+
+/// `83 /5 ib` : `SUB r/m32 imm8` : subtract imm8 sign extended to 32-bit from r/m32
 pub const fn sub_imm8_from_r32(dest: Reg, ib: i8) -> [u8; 3] {
     let modrm = modrm_ext(ModRm::Register(dest), 5);
     let [ib] = i8::to_le_bytes(ib);
     [0x83, modrm, ib]
 }
 
-// `81 /5 id` : `SUB r/m32 imm32` : subtract imm32 from r/m32
+/// `81 /5 id` : `SUB r/m32 imm32` : subtract imm32 from r/m32
 pub const fn sub_imm32_from_r32(dest: Reg, id: i32) -> [u8; 6] {
     let modrm = modrm_ext(ModRm::Register(dest), 5);
     let [b0, b1, b2, b3] = i32::to_le_bytes(id);
     [0x81, modrm, b0, b1, b2, b3]
 }
 
-// `REX.W 81 /5 id` : `SUB r/m64 imm32` : subtract imm32 sign extended to 64-bits from r/m64
+// 64-bit
+
+/// `REX.W 81 /5 id` : `SUB r/m64 imm32` : subtract imm32 sign extended to 64-bits from r/m64
 pub const fn sub_imm32_from_r64(dest: Reg, id: i32) -> [u8; 7] {
     let modrm = modrm_ext(ModRm::Register(dest), 5);
     let [b0, b1, b2, b3] = i32::to_le_bytes(id);
@@ -571,44 +609,52 @@ pub const fn sub_imm32_from_r64(dest: Reg, id: i32) -> [u8; 7] {
 //                   MOV
 // ========================================
 
-// `C6 /0 ib` : `MOV r/m8 imm8` : move imm8 to r/m8
+// 8-bit
+
+/// `8A /r`: `MOV r8 r/m8` : move r/m8 to r8
+pub const fn mov_sib8_to_r8(src: Sib, dest: Reg) -> [u8; 3] {
+    let modrm = modrm_reg(ModRm::Indirect(RmI::Sib), dest);
+    [0x8A, modrm, src.sib()]
+}
+
+/// `C6 /0 ib` : `MOV r/m8 imm8` : move imm8 to r/m8
 pub const fn mov_imm8_to_r8(dest: Reg, ib: u8) -> [u8; 3] {
     let modrm = modrm_ext(ModRm::Register(dest), 0);
     [0xC6, modrm, ib]
 }
+/// `C6 /0 ib` : `MOV r/m8 imm8` : move imm8 to r/m8
 pub const fn mov_imm8_to_sib8(dest: Sib, ib: u8) -> [u8; 4] {
     let modrm = modrm_ext(ModRm::Indirect(RmI::Sib), 0);
     [0xC6, modrm, dest.sib(), ib]
 }
+/// `C6 /0 ib` : `MOV r/m8 imm8` : move imm8 to r/m8
 pub const fn mov_imm8_to_sib8_disp8(dest: Sib, disp: i8, ib: u8) -> [u8; 5] {
     let modrm = modrm_ext(ModRm::IndirectDisp8(RmID::Sib), 0);
     let [disp] = i8::to_le_bytes(disp);
     [0xC6, modrm, dest.sib(), disp, ib]
 }
+/// `C6 /0 ib` : `MOV r/m8 imm8` : move imm8 to r/m8
 pub const fn mov_imm8_to_sib8_disp32(sib: Sib, disp: i32, ib: u8) -> [u8; 8] {
     let modrm = modrm_ext(ModRm::IndirectDisp32(RmID::Sib), 0);
     let [b0, b1, b2, b3] = i32::to_le_bytes(disp);
     [0xC6, modrm, sib.sib(), b0, b1, b2, b3, ib]
 }
 
-// `8A /r`: `MOV r8 r/m8` : move r/m8 to r8
-pub const fn mov_sib8_to_r8(src: Sib, dest: Reg) -> [u8; 3] {
-    let modrm = modrm_reg(ModRm::Indirect(RmI::Sib), dest);
-    [0x8A, modrm, src.sib()]
-}
+// 64-bit
 
-// `REX.W 89 /r` : `MOV r/m64 r64` : move r64 to r/m64
+/// `REX.W 89 /r` : `MOV r/m64 r64` : move r64 to r/m64
 pub const fn mov_r64_to_r64(src: Reg, dest: Reg) -> [u8; 3] {
     let modrm = modrm_reg(ModRm::Register(dest), src);
     [REXW, 0x89, modrm]
 }
 
-// `REX.W C7 /0 id` : `MOV r/m64 imm32` : move imm32 sign extended to 64-bits to r/m64
+/// `REX.W C7 /0 id` : `MOV r/m64 imm32` : move imm32 sign extended to 64-bits to r/m64
 pub const fn mov_imm32_to_r64(dest: Reg, id: i32) -> [u8; 7] {
     let modrm = modrm_ext(ModRm::Register(dest), 0);
     let [b0, b1, b2, b3] = i32::to_le_bytes(id);
     [REXW, 0xC7, modrm, b0, b1, b2, b3]
 }
+/// `REX.W C7 /0 id` : `MOV r/m64 imm32` : move imm32 sign extended to 64-bits to r/m64
 pub const fn mov_imm32_to_sib64(dest: Sib, id: i32) -> [u8; 8] {
     let modrm = modrm_ext(ModRm::Indirect(RmI::Sib), 0);
     let [b0, b1, b2, b3] = i32::to_le_bytes(id);
@@ -619,56 +665,56 @@ pub const fn mov_imm32_to_sib64(dest: Sib, id: i32) -> [u8; 8] {
 //                   MISC
 // ========================================
 
-// `F6 /4`: `MUL r/m8` : multiply al with r/m8 into ax
+/// `F6 /4`: `MUL r/m8` : multiply al with r/m8 into ax
 pub const fn mul_al_with_sib8(src: Sib) -> [u8; 3] {
     let modrm = modrm_ext(ModRm::Indirect(RmI::Sib), 4);
     [0xF6, modrm, src.sib()]
 }
 
-// `31 /r`: `XOR r/m64 r64` : performs r/m64 xor r64 into r/m64
+/// `31 /r`: `XOR r/m64 r64` : performs r/m64 xor r64 into r/m64
 pub const fn xor_r64_r64(src: Reg, dest: Reg) -> [u8; 3] {
     let modrm = modrm_reg(ModRm::Register(dest), src);
     [REXW, 0x31, modrm]
 }
 
-// `80 /7 ib` : `CMP r/m8 imm8` : compare r/m8 with imm8
+/// `80 /7 ib` : `CMP r/m8 imm8` : compare r/m8 with imm8
 pub const fn cmp_sib8_with_imm8(src: Sib, ib: u8) -> [u8; 4] {
     let modrm = const { modrm_ext(ModRm::Indirect(RmI::Sib), 7) };
     [0x80, modrm, src.sib(), ib]
 }
 
-// `83 /7 ib` : `CMP r/m32 imm8` : compare r/m32 with imm8
+/// `83 /7 ib` : `CMP r/m32 imm8` : compare r/m32 with imm8
 pub const fn cmp_r32_with_imm8(src: Reg, ib: i8) -> [u8; 3] {
     let modrm = modrm_ext(ModRm::Register(src), 7);
     let [ib] = i8::to_le_bytes(ib);
     [0x83, modrm, ib]
 }
 
-// `0F 84 cd` : `JZ rel32` : jump rel32 if zero
+/// `0F 84 cd` : `JZ rel32` : jump rel32 if zero
 pub const fn jz_rel32(cd: i32) -> [u8; 6] {
     let [b0, b1, b2, b3] = i32::to_le_bytes(cd);
     [0x0F, 0x84, b0, b1, b2, b3]
 }
 
-// `75 cd` : `JNZ rel8` : jump rel8 if not zero
+/// `75 cd` : `JNZ rel8` : jump rel8 if not zero
 pub const fn jnz_rel8(cd: i8) -> [u8; 2] {
     let [cd] = i8::to_le_bytes(cd);
     [0x75, cd]
 }
 
-// `0F 85 cd` : `JNZ rel32` : jump rel32 if not zero
+/// `0F 85 cd` : `JNZ rel32` : jump rel32 if not zero
 pub const fn jnz_rel32(cd: i32) -> [u8; 6] {
     let [b0, b1, b2, b3] = i32::to_le_bytes(cd);
     [0x0F, 0x85, b0, b1, b2, b3]
 }
 
-// `FF /6`: `PUSH r64` : push r64 onto the stack
+/// `FF /6`: `PUSH r64` : push r64 onto the stack
 pub const fn push_r64(src: Reg) -> [u8; 2] {
     let modrm = modrm_ext(ModRm::Register(src), 6);
     [0xFF, modrm]
 }
 
-// `8F /0`: `POP r64` : pop r64 off the stack
+/// `8F /0`: `POP r64` : pop r64 off the stack
 pub const fn pop_r64(dest: Reg) -> [u8; 2] {
     let modrm = modrm_ext(ModRm::Register(dest), 0);
     [0x8F, modrm]
